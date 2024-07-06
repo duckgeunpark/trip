fccecc29e9c5d21b4e8b0fcf399da3e791ae173f
duckgeunpark <ejrrms0064@gmail.com>
2024-07-06 23:52:36 +0900

    수정

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-# API KEY를 환경변수로 관리하기 위한 설정 파일
-# 설치: pip install python-dotenv
+from fastapi import FastAPI
+from pydantic import BaseModel
 from dotenv import load_dotenv
 import os
 from langchain_teddynote import logging
-
+from langchain_openai import ChatOpenAI
 # API KEY 정보로드
 load_dotenv()
-
+# FastAPI 앱 생성
+app = FastAPI()
 # 프로젝트 이름을 입력합니다.
 logging.langsmith("apitester")
-
-from langchain_openai import ChatOpenAI
-
 # 객체 생성
 llm = ChatOpenAI(
     temperature=0.1,  # 창의성 (0.0 ~ 2.0)
     model_name="gpt-4o",  # 모델명
+
 )
-
-# 질의내용
-question = "대한민국의 수도는 어디인가요?"
-
-# 질의
-print(f"[답변]: {llm.invoke(question)}")
+class Question(BaseModel):
+    question: str
+    
+# 엔드포인트 정의
+@app.post("/ask")
+async def ask_question(question: Question):
+    answer = llm.invoke(question.question)
+    return {"답변": answer}
